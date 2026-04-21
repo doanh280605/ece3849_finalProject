9553be1a4039ea22386523ac7ec3522d3d0c6385
Doanh Phung <dangdoanh.phung@gmail.com>
2026-04-20 22:36:48 -0400

    Update main.cpp file

--- main.cpp
+++ main.cpp
@@ -3,49 +3,37 @@
 
 extern "C" {
 #include "driverlib/fpu.h"
-#include "driverlib/interrupt.h"
 #include "driverlib/sysctl.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
 }
 
-#include "app_config.h"
 #include "debug_uart.h"
 #include "motor.h"
-#include "safety.h"
 #include "sensor.h"
 #include "system_tasks.h"
 #include "wireless.h"
 
-static void ConfigureSystemClock(void);
-
 int main(void)
 {
-    IntMasterDisable();
     FPUEnable();
     FPULazyStackingEnable();
 
-    ConfigureSystemClock();
+    gSystemClockHz = SysCtlClockFreqSet(
+        SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN |
+        SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480,
+        120000000);
 
     DebugUART_Init();
-    Sensor_Init();
     Motor_Init();
     Wireless_Init();
-    Safety_Init();
+    Sensor_Init();
+
     Tasks_Create();
 
-    IntMasterEnable();
     vTaskStartScheduler();
 
     while (true) {
     }
-}
-
-static void ConfigureSystemClock(void)
-{
-    gSystemClockHz = SysCtlClockFreqSet(
-        SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN |
-        SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480,
-        kSystemClockHz);
 }
